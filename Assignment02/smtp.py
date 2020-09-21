9a0287cb4b9e34ddcfaeb9678492a85da168e54d
DakshaNagre <dnagre@iu.edu>
2020-09-21 17:00:06 -0400

    Assignment02

--- Assignment02/smtp.py
+++ Assignment02/smtp.py
@@ -1,14 +1,20 @@
-#!/usr/bin/env python3
-
 import os
 import time
 import argparse
+import smtplib
 
 # Entry function for sending mail via SMTP.  The input arguments allow you to
 # contruct a well-formed email message and send it a specific server.
 def send_mail(server, faddr, taddr, msg):
     print (server, faddr, taddr, msg)
-    pass
+    server = smtplib.SMTP(server)
+    message = "From: " + faddr + "\n"
+    subject = "Exploring the Simple Mail Transfer Protocol"
+    message = 'Subject: {}\n\n{}'.format(subject, msg)
+    # print("message : " + message)
+    server.sendmail(faddr, taddr, message)
+    server.quit()
+
 
 def main():
     parser = argparse.ArgumentParser(description="SICE Network SMTP Client")
@@ -20,12 +26,12 @@ def main():
                         help='Receiver address')
     parser.add_argument('message', type=str,
                         help='Message text to send')
-    
+
     args = parser.parse_args()
-    
+
     send_mail(args.mail_server, args.from_address,
               args.to_address, args.message)
+
     
 if __name__ == "__main__":
     main()
-    
